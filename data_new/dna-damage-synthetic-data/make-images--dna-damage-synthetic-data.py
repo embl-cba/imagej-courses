740ca7bcae8b9cf00a2130095745d191cca3a3ee
tischi <tischitischer@gmail.com>
2017-03-17 21:13:50 +0100

    intensity quantification

--- data_new/dna-damage-synthetic-data/make-images--dna-damage-synthetic-data.py
+++ data_new/dna-damage-synthetic-data/make-images--dna-damage-synthetic-data.py
@@ -1,10 +1,12 @@
 #
-# Author: Christian 'Tischi' Tischer, christian.tischer@embl.de
+# Authors: 
+# Christian 'Tischi' Tischer; christian.tischer@embl.de
 #
-# Time-lapse data of laser induced nuclear damage
+# Simluated data of the recruitement of DNA repair enzymes to 
+# laser induced DNA damage
 # 
 # Requirements
-# - RandomJ: RandomJ is available from the ImageScience update site.
+# - RandomJ: Add the ImageScience update site.
 #
 
 from ij import IJ
@@ -47,11 +49,11 @@ def blur_and_noise(imp, blur, noise):
   return(imp)
 
 
-def draw_cell(imp, i, nucleus_total, damage_total, title):
+def draw_cell(imp, i, nucleus_total, damage_total, title, noise):
   imp.append(impBG.duplicate()); 
   draw_nucleus( imp[i], nucleus_total / nucleus_area )
   draw_damage( imp[i], damage_total / damage_area )
-  imp[i] = blur_and_noise(imp[i], 2, 1)
+  imp[i] = blur_and_noise(imp[i], 2, noise)
   imp[i].show()
   imp[i].setTitle(title)
   return(imp)
@@ -79,15 +81,15 @@ i = -1;
 # No damage
 
 damage_total = 0
-i = i + 1; imps = draw_cell(imps, i, nucleus_total, damage_total, "Control")
+i = i + 1; imps = draw_cell(imps, i, nucleus_total, damage_total, "Control", 1)
 
 # With damage, clear signal in the damage region, same total intensity as "No damage"
 # => recruitement to damage site
 
 damage_total = nucleus_total * 0.1
-nucleus_total = nucleus_total - damage_total
+nucleus_total_here = nucleus_total - damage_total
 
-i = i + 1; imps = draw_cell(imps, i, nucleus_total, damage_total, "Damaged")
+i = i + 1; imps = draw_cell(imps, i, nucleus_total_here, damage_total, "Damaged", 1)
 
 
 # With damage, clear signal in the damage region, less total intensity as "No damage"
@@ -97,9 +99,9 @@ i = i + 1; imps = draw_cell(imps, i, nucleus_total, damage_total, "Damaged")
 
 bleaching = 0.8
 damage_total = nucleus_total * bleaching * 0.1
-nucleus_total = nucleus_total * bleaching - damage_total
+nucleus_total_here = nucleus_total * bleaching - damage_total
 
-i = i + 1; imps = draw_cell(imps, i, nucleus_total, damage_total, "Damaged and treated 01")
+i = i + 1; imps = draw_cell(imps, i, nucleus_total_here, damage_total, "Damaged and treated 01", 1)
 
 
 # "Treated": with damage, same total intensity as "No damage", but less signal in the damage site
@@ -107,8 +109,37 @@ i = i + 1; imps = draw_cell(imps, i, nucleus_total, damage_total, "Damaged and t
 
 bleaching = 1.0 # no bleaching
 damage_total = nucleus_total * bleaching * 0.05
-nucleus_total = nucleus_total * bleaching - damage_total
-i = i + 1; imps = draw_cell(imps, i, nucleus_total, damage_total, "Damaged and treated 02")
+nucleus_total_here = nucleus_total * bleaching - damage_total
+i = i + 1; imps = draw_cell(imps, i, nucleus_total_here, damage_total, "Damaged and treated 02", 1)
+
+# Multiple damage sites close-by
+# => Median filter becomes difficult
+
+damage_total = nucleus_total * 0.1
+nucleus_total_here = nucleus_total
+
+i = i + 1; imps.append(impBG.duplicate()); 
+draw_nucleus( imps[i], nucleus_total / nucleus_area )
+## Multiple damage sites
+
+for j in range(-3,4,1):
+  ll = l*((10.0-abs(j))/10.0)
+  imps[i].setRoi(c+d/2+j*d/9,int(c+(d-ll)/2.0),r,int(ll));
+  IJ.run(imps[i], "Add...", "value="+str(0.9*damage_total / damage_area));
+
+imps[i] = blur_and_noise(imps[i], 2, 1)
+imps[i].show()
+imps[i].setTitle("Multiple damage sites")
+
 
 # With damage and high noise
 # => tophat filter does not work very well
+
+damage_total = nucleus_total * 0.1
+nucleus_total_here = nucleus_total - damage_total
+i = i + 1; imps = draw_cell(imps, i, nucleus_total_here, damage_total, "Damaged Noisy", 3)
+
+# todo: find a way to add more noise...
+
+
+
